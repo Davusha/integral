e0dedb4ac0fd4d5760ae3e7122eb010d078c2614
Davusha <74594331+Davusha@users.noreply.github.com>
2024-01-17 19:09:35 +0000

    init

---
+++ client/build/CMakeFiles/projectClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectClient.dir/main.c.o"
+  "CMakeFiles/projectClient.dir/main.c.o.d"
+  "projectClient"
+  "projectClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projectClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikita/labLinux/labs/lab5/server
+# Build directory: /home/nikita/labLinux/labs/lab5/server/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
